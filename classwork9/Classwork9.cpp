0e5715173cbb10fb0ee675fb1b4a28d1fe70ea54
Glenn <glenndavidmah@gmail.com>
2024-09-15 19:40:49 -0700

    adding work files

---
+++ classwork9/Classwork9.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int experience = 1;
+	bool hasDegree = false;
+
+	if (experience > 1 || hasDegree == true) {
+		cout << "you are eligable for citizenship" << endl;
+	}
+	else {
+		cout << "you are not eligable for citizenship" << endl;
+	}
+
+
+}
+
+
